5f5ad9a6149c803565619d4d01d44d056660f1ce
Jiaming Guo <gjm.mailbox2@gmail.com>
2021-01-16 20:06:20 -0500

    second commit

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const multer = require("multer");
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use(express.static(path.join(__dirname, "public")));
+
+const storage = multer.diskStorage({
+    destination: "./public/photos/",
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
 });
 
-app.listen(HTTP_PORT);
+const upload = multer({ storage: storage });
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/upload.html"));
+}); 
+
+app.post("/", upload.single("photo"), (req, res) => {
+    const formFile = req.file;
+    const dataReceived = "Your submission was received:<br/>" + "This is the image you sent:<br/><img src='/photos/" + formFile.filename + "'/>";
+    res.send(dataReceived);
+});
 
 app.use((req, res) => {
     res.status(404).send("Page Not Found");
+});
+
+app.listen(HTTP_PORT, ()=>{
+    console.log("listening on: " + HTTP_PORT);
 });
